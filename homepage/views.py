d72728bc6dcf5f461451fef4d6a492bbe9992bb4
Jeremiah Moore <moore.jeremiah.mark@gmail.com>
2017-04-29 18:45:16 +0000

    added posts to the portfolio webpage

--- homepage/views.py
+++ homepage/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from homepage.models import Post
+from collection.forms import ContactForm
 
 def index(request):
     data = {'myData': "Hello world more to come"}
     return render(request, 'homepage/index.html', data )
     
 def contact(request):
-    data = {'myData': "Hello world more is on the way"}
-    return render(request, 'homepage/contact.html', data )
+    form_class=ContactForm
+    return render(request, 'homepage/contact.html', {'form':form_class,})
 
 def portfolio(request):
-    data = {'myData': "Hello world more in the works"}
-    return render(request, 'homepage/portfolio.html', data )
+    posts = Post.objects.order_by("updated_date")
+    return render(request, 'homepage/portfolio.html', {'post': posts} )
